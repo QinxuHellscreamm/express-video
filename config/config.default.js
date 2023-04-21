af759ae15370900ddd802ed36f1edbd1d3195fc7
李思 <969417321@qq.com>
2023-04-21 16:55:48 +0800

    1

--- config/config.default.js
+++ config/config.default.js
@@ -2,7 +2,7 @@
  * 默认配置
  */
 module.exports.uuid = '9125a8dc-52ee-365b-a5aa-81b0b3681cf6'
-module.exports.mongoPath = 'mongodb://39.106.21.210:27017/express-video'
+module.exports.mongoPath = 'mongodb://0.0.0.0:27017/express-video'
 module.exports.redisClient = {
     port:6379,
     host:'39.106.21.210',
