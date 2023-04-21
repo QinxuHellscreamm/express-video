f443468a853513fe3cc9281caa0241e1112cca79
李思 <969417321@qq.com>
2023-04-21 17:38:16 +0800

    忽略文件

--- config/config.default.js
+++ config/config.default.js
@@ -1,8 +1,10 @@
 /**
  * 默认配置
  */
+require('dotenv').config()
 module.exports.uuid = '9125a8dc-52ee-365b-a5aa-81b0b3681cf6'
-module.exports.mongoPath = 'mongodb://39.106.21.210:27017/express-video'
+console.log('hah',process.env);
+module.exports.mongoPath = 'mongodb://'+process.env.DB_HOST ? process.env.DB_HOST : '0.0.0.0' +':27017/express-video'
 module.exports.redisClient = {
     port:6379,
     host:'39.106.21.210',
