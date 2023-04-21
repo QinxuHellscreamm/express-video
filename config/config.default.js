7260b6a2df7d3fc58a8dbce4f513aaf77e69ad04
李思 <969417321@qq.com>
2023-04-21 17:52:34 +0800

    配置mongodb环境变量

--- config/config.default.js
+++ config/config.default.js
@@ -3,8 +3,8 @@
  */
 require('dotenv').config()
 module.exports.uuid = '9125a8dc-52ee-365b-a5aa-81b0b3681cf6'
-console.log('hah',process.env);
-module.exports.mongoPath = 'mongodb://'+process.env.DB_HOST ? process.env.DB_HOST : '0.0.0.0' +':27017/express-video'
+const host = process.env.DB_HOST ? process.env.DB_HOST : '0.0.0.0'
+module.exports.mongoPath = 'mongodb://'+host +':27017/express-video'
 module.exports.redisClient = {
     port:6379,
     host:'39.106.21.210',
